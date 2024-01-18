345b41d0bb164e20cf6b4214d34592bd7235a840
Admin <Admin@DESKTOP-JFHQ5Q0>
2024-01-18 07:31:15 +0700

    update

--- SignalRChat/SignalRChat/Program.cs
+++ SignalRChat/SignalRChat/Program.cs
@@ -1,7 +1,11 @@
+using SignalRChat.Hubs;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddSignalR();
 
 var app = builder.Build();
 
@@ -21,5 +25,5 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapRazorPages();
-
+app.MapHub<ChatHub>("/chatHub");
 app.Run();
